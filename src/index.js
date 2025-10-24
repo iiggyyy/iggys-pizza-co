5f68531b05a2aa69b302fe7224cf5b0e5ca72e0c
iiggyyy <24013686@myrp.edu.sg>
2025-10-24 10:50:24 +0800

    Initial commit - Iggy's Pizza Co React app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,86 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Header() {
+  return (
+    <header>
+      <h1>Iggy's Pizza Co.</h1>
+      <h2>OUR MENU</h2>
+      <p>Authentic Italian cuisine, all from our stone oven</p>
+    </header>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.src} alt={props.name} width="150" />
+      <h3>{props.name}</h3>
+      <p>{props.ingredients}</p>
+      <p>{props.price}</p>
+    </div>
+  );
+}
+
+function App() {
+  const pizzas = [
+    {
+      name: "Focaccia",
+      ingredients: "Bread with Italian olive oil and rosemary",
+      price: 6,
+      src: "/pizzas/focaccia.jpg"
+    },
+    {
+      name: "Pizza Margherita",
+      ingredients: "Tomato and mozzarella",
+      price: 10,
+      src: "/pizzas/margherita.jpg"
+    },
+    {
+      name: "Pizza Spinaci",
+      ingredients: "Tomato, mozzarella, spinach, and ricotta cheese",
+      price: 12,
+      src: "/pizzas/spinaci.jpg"
+    },
+    {
+      name: "Pizza Funghi",
+      ingredients: "Tomato, mozzarella, mushrooms, and onion",
+      price: 12,
+      src: "/pizzas/funghi.jpg"
+    },
+    {
+      name: "Pizza Salamino",
+      ingredients: "Tomato, mozzarella, and pepperoni",
+      price: 15,
+      src: "/pizzas/salamino.jpg"
+    },
+    {
+      name: "Pizza Prosciutto",
+      ingredients: "Tomato, mozzarella, ham, arugula, and burrata cheese",
+      price: 18,
+      src: "/pizzas/prosciutto.jpg"
+    }
+  ];
+
+  return (
+    <div>
+      <Header />
+      <div className="menu">
+        {pizzas.map(function (pizza) {
+          return (
+            <Pizza
+              key={pizza.name}
+              name={pizza.name}
+              ingredients={pizza.ingredients}
+              price={pizza.price}
+              src={pizza.src}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
